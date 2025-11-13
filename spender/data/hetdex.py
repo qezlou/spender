501d81bcf70c5d8aa4aea3a533938e3223ad313e
qezlou <mahdiqezlou@gmail.com>
2025-11-13 10:10:41 -0600

    data_laoder: multiple files loading
    Fiest attempts, not working yet

--- spender/data/hetdex.py
+++ spender/data/hetdex.py
@@ -11,6 +11,8 @@ import torch
 import pickle
 from torch.utils.data import DataLoader
 from ..instrument import Instrument
+import ray
+import re
 
 
 class HETDEX(Instrument):
@@ -71,58 +73,51 @@ class HETDEX(Instrument):
         -------
         :class:`torch.utils.data.DataLoader`
         """
-        raw = self.load_raw_file(dir, file_name)
-        spec = raw["spec"]
-        w = raw["ivar"]  # weight
-        z = raw["z"]
-        shotids = raw['shotids']
-        n = len(spec)
-        split = int(split_ratio * n)
-        norm = raw['norm']
-        if which == "train":
-            spec, w, z, shotids = spec[:split], w[:split], z[:split], shotids[:split]
-        elif which == "valid":
-            spec, w, z, shotids = spec[split:], w[split:], z[split:], shotids[split:]
-        
-        if get_shotids:
-            dataset = torch.utils.data.TensorDataset(spec, w, z, shotids)
+        shards = self.get_shard_paths(dir, file_name)
+        if len(shards) > 1:
+            num_blocks = 32
         else:
-            dataset = torch.utils.data.TensorDataset(spec, w, z)
-        torch.manual_seed(seed)
-        return DataLoader(dataset, batch_size=batch_size, shuffle=shuffle), norm
+            num_blocks = 1
+        dataset = ray.data.read_numpy(shards, override_num_blocks=num_blocks)
+        train_ds, valid_ds = dataset.random_shuffle().split_proportionately([1-split_ratio, split_ratio], seed=seed)
 
-    def load_raw_file(self, data_dir, file_name='all_calfibs.h5', normalize=False):
-        """Load pre-saved HETDEX fiber spectra in h5 format
+        if which == "train":
+            return train_ds.iter_torch_batches(batch_size=batch_size, shuffle=shuffle)
+        elif which == "valid":
+            return valid_ds.iter_torch_batches(batch_size=batch_size, shuffle=shuffle)
+
+    def get_shard_paths(self, dir, file_name):
+        """Get list of shard file paths
 
         Parameters
         ----------
+        dir: string
+            Root directory for data storage
         file_name: string
-            Path to local HDF5 file containing the calfib data
+            Name to specify which batch files to load
 
         Returns
         -------
-        data: dict
-            Dictionary with keys 'spec', 'ivar', 'mask', 'z' containing the
-            spectra, inverse variance weights, mask, and redshift arrays.
-            Redshift is set to zero as it is irrelevant for HETDEX data.
+        list of strings
+            List of shard file paths
         """
-        file_name = os.path.join(data_dir, 'fib_spec', file_name)
-        with h5py.File(file_name, 'r') as f:
-            spec = torch.from_numpy(f['calfib'][:,65:916])
-            calfibe = f['calfibe'][:,65:916]
-            shotids = torch.from_numpy(f['shotids'][:])
-            calfibe[calfibe <= 0] = np.inf  # avoid zero or negative fluxes
-            ivar = torch.from_numpy(1.0 / calfibe**2)
-            # We are not using the mask here!! 
-            mask = torch.from_numpy(np.where(f['calfibe'][:,65:916] <= 0, 1, 0))
-            z = torch.from_numpy(np.zeros_like(f['calfib'][:,0]))
-        # Normalize the spectra using the median in the range 4300-5200AA
-        sel = (self.wave_obs >= 4300) & (self.wave_obs <= 5200)
-        if normalize:
-            norm = torch.median(spec[:, sel])
-        else:
-            norm = torch.ones(spec.shape[1])
-        spec = spec  / norm
-        ivar = ivar * (norm**2).unsqueeze(0)
+        shard_paths = glob.glob(os.path.join(dir, 'fib_spec', file_name, '*.h5'))
+        # Make sure ther are in-order, i.e. newest date-shots are first
+        # Files are saved in reverse date order
+        tags = [int(re.search(r'c(\d+)', fname).group(1)) for fname in shard_paths]
+        shard_paths = [x for _, x in sorted(zip(tags, shard_paths))]
+        return shard_paths
 
-        return {'spec': spec, 'ivar': ivar, 'mask': mask, 'z': z, 'shotids': shotids, 'norm': norm}
+    def read_h5_batch(self, shard_path):
+        with h5py.File(shard_path, "r") as f:
+            spec = f["calfib"][:]
+            err = f["calfibe"][:]
+            shotids = f["shotids"][:]
+            err[err <= 0] = np.inf
+            ivar = 1.0 / err**2
+            z = np.zeros(spec.shape[0])
+            norm = np.ones(spec.shape[0])
+            # We don't use the mask here, yet!!
+            mask = np.where(err <= 0, 1, 0)
+            return {"spec": spec, "ivar": ivar, "mask": mask, "z": z, "shotids": shotids, "norm": norm}
+    
